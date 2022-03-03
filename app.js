38130101c4c84f4305a63d216beca54689681ac7
Sajan M <sajanmurali@ymail.com>
2022-03-03 12:50:10 +0530

    staged 1

---
+++ app.js
@@ -0,0 +1,49 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var adminRouter = require('./routes/admin');
+var usersRouter = require('./routes/users');
+var fileUpload = require ('express-fileupload');
+const { hasSubscribers } = require('diagnostics_channel');
+const {engine} = require('express-handlebars');
+var app = express();
+var hbs = require('hbs');
+
+// view engine setup
+app.set('view engine', 'html');
+app.engine('html', require('hbs').__express);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(fileUpload());
+
+app.use('/', usersRouter);
+app.use('/admin',adminRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/admin.js
@@ -0,0 +1,55 @@
+const cookieParser = require('cookie-parser');
+var express = require('express');
+const res = require('express/lib/response');
+const app = require('../app');
+
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+
+  let Products = [
+    {
+      Name: "Gown",
+      Category: "Dress",
+      Discription: "attires are mean to be for confident",
+      Image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRws4zT4Lk79_rxBQDXcxTG1IS5oUvKtBk7SQ&usqp=CAU"
+    },
+    {
+      Name: "Gown",
+      Category: "Dress",
+      Discription: "attires are mean to be for confident",
+      Image: "https://n.nordstrommedia.com/id/sr3/2e036672-dd58-4404-8d44-12bd64b08e91.jpeg?h=365&w=240&dpr=2"
+    },
+    {
+      Name: "Gown",
+      Category: "Dress",
+      Discription: "attires are mean to be for confident",
+      Image: "https://dreamdressexpress.com/wp-content/uploads/2020/08/ew120108_red_f.jpg"
+    },
+    {
+      Name: "Gown",
+      Category: "Dress",
+      Discription: "attires are mean to be for confident",
+      Image: "https://assets.ajio.com/medias/sys_master/root/20210403/bsM4/606867797cdb8c1f147522ca/-473Wx593H-461088032-blue-MODEL.jpg"
+    }
+
+  ]
+
+
+  res.render('Admin/Adviews', { admin: true, Products });
+});
+router.get('/Add-Product', function (req, res) {
+  res.render('Admin/Add-Product')
+});
+
+
+router.post('/Add-Product', (req, res) => {
+  console.log(req.body);
+  console.log(req.files.Image);
+
+
+});
+
+
+module.exports = router;
